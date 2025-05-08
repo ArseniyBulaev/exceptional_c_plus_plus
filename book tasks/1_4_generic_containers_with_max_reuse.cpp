33f61921b85b678995bc029ab62f3f4464285965
Arseny Bulaev <seni190999@mail.ru>
2025-05-08 21:47:46 +0300

    copy operator

---
+++ book tasks/1_4_generic_containers_with_max_reuse.cpp
@@ -0,0 +1,67 @@
+#include "doctest/doctest.h"
+#include <cstddef>
+#include <algorithm>
+#include <initializer_list>
+
+/* TO DO:
+We need max reuse of copy constructor and copy operator
+1. Write copy constructor 
+2. Write copy operator + 
+*/
+
+template<typename T, size_t size>
+class fixed_vector
+{
+    public:
+        typedef T* iterator;
+        typedef const T* const_iterator;
+        iterator begin() {return v_;}
+        iterator end() {return v_ + size;}
+        const_iterator begin() const {return v_;}
+        const_iterator end() const {return v_ + size;}
+        fixed_vector<T, size>() {}
+        fixed_vector<T, size>(std::initializer_list<T> ilist){
+            std::copy(ilist.begin(), ilist.end(), begin());
+        }
+        fixed_vector<T, size> & operator=(const fixed_vector<T, size> & other){
+            // if vector sizes aren't equal, is there a compilation error? Yes
+            std::copy(other.begin(), other.end(), begin());
+            return *this;
+        }
+        bool operator==(const fixed_vector<T, size> & other){
+            for (size_t i = 0; i < size; ++i){
+                if (v_[i] != other.v_[i]) return false;
+            }
+            return true;
+        }
+
+        bool operator!=(const fixed_vector<T, size> & other){
+           return operator==(other);
+        }
+
+    private:
+        T v_[size];
+};
+
+
+
+
+TEST_CASE("assign operator"){
+    
+    SUBCASE("empty vectors"){
+        const size_t size = 5;
+        fixed_vector<int, size> fv_1;
+        fixed_vector<int, size> fv_2;
+        fv_1 = fv_2;
+        CHECK(fv_1 == fv_2);
+    }
+
+    SUBCASE("non empty vectors"){
+        const size_t size = 5;
+        fixed_vector<int, size> fv_1 = {1, 2, 3, 4, 5};
+        fixed_vector<int, size> fv_2 = {3, 4, 5, 6, 7};
+        fv_1 = fv_2;
+        CHECK(fv_1 == fv_2);
+    }
+    
+}
